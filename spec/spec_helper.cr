23d854f17b9c083b24b7b389b90fd7337e8c70da
qbart <wojtowicz.bartlomiej@gmail.com>
2021-10-22 22:55:27 +0200

    Update path in specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,11 +1,10 @@
 ENV["KEMAL_ENV"] = "test"
 require "spec"
 require "spec-kemal"
-require "../src/server"
+require "../src/main"
 
 def json_headers
-  HTTP::Headers {
-    "Content-type" => "application/json"
+  HTTP::Headers{
+    "Content-type" => "application/json",
   }
 end
-
